07e2b488b88cc1a06c2f2c493818f6225f2fe6d6
Afonso_miqueias <afonsogroove@gmail.com>
2023-03-01 20:03:08 +0100

    update49

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.home),  # home
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name='recipes-home'),  # home
+    path('recipes/<int:id>/', views.recipe, name='recipes-recipe'),
 
 
 ]
